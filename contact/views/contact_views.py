4b34cf23fb816a7f42c294851a6f8683cfdb282e
atilacapovilla <capovillaatila@gmail.com>
2024-11-15 17:54:48 -0300

    Querysets com filter, order_by e fatiamento

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -3,7 +3,17 @@ from contact.models import Contact
 
 
 def index(request):
-    contacts = Contact.objects.all()
+    # Seleciona todos os contatos com order decrescente de id
+    # contacts = Contact.objects.all().order_by('-id')
+
+    # Seleciona filtra os contatos com o campo show True
+    # contacts = Contact.objects.filter(show=True).order_by('-id')
+
+    # Seleciona filtra os contatos com o campo show True mostra os 5 primeiros
+    contacts = Contact.objects.filter(show=True).order_by('-id')[0:5]
+    # print(contacts.query)
+
+    # cria a variável de contexto
     context = {
         'contacts': contacts,
     }
