69b8d3aad91bc58d6847c65d34b9569a90f2b024
atilacapovilla <capovillaatila@gmail.com>
2024-12-04 17:32:28 -0300

    Trabalhando com Imagens

--- contact/forms.py
+++ contact/forms.py
@@ -9,6 +9,13 @@ from contact.models import Contact
 
 
 class ContactForm(forms.ModelForm):
+    picture = forms.ImageField(
+        widget=forms.FileInput(
+            attrs={
+                'accept': 'image/*',
+            }
+        )
+    )
 
     first_name = forms.CharField(
         widget=forms.TextInput(
@@ -40,7 +47,7 @@ class ContactForm(forms.ModelForm):
     class Meta:
         model = Contact
         fields = (
-            'first_name', 'last_name', 'phone', 'email', 'description', 'category'
+            'first_name', 'last_name', 'phone', 'email', 'description', 'category', 'picture'
         )
         # widgets = {
         #     'first_name': forms.TextInput(
